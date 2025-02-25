3dba1753d476f73116cd78bda7038dfb5d7322b5
Jatin Singh <jatinsingh95406@gmail.com>
2025-02-25 11:52:15 +0530

    adding function

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ arr.forEach(button => {
         handleButtonInput(e.target.innerHTML);
     });
 });
+
+
+// Function to handle input based on button content
+function handleButtonInput(value) {
+    if (value === '=') {
+        string = eval(string);
+        input.value = string;
+    } else if (value === 'AC') {
+        string = "";
+        input.value = string;
+    } else if (value === 'DEL') {
+        string = string.substring(0, string.length - 1);
+        input.value = string;
+    } else {
+        string += value;
+        input.value = string;
+    }
+}
