f8a25059908210c33acf8d9e7e5ece57c3acc5d5
Jatin Singh <jatinsingh95406@gmail.com>
2025-02-25 11:49:33 +0530

    adding string

---
+++ main.js
@@ -0,0 +1,11 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        handleButtonInput(e.target.innerHTML);
+    });
+});
